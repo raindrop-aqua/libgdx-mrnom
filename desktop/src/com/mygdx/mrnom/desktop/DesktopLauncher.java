2e7ed943bbe7a8c6b9790bae69a61b7f0a6f08c8
渥美政廣 <raindrop.aqua@gmail.com>
2016-03-27 23:33:43 +0900

    first commit.

---
+++ desktop/src/com/mygdx/mrnom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.mrnom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.mrnom.MrNom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MrNom(), config);
+	}
+}
