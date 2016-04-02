fe56d302150f7548a7d8f229689c470ee82e0fc2
渥美政廣 <raindrop.aqua@gmail.com>
2016-04-02 17:08:47 +0900

    modelクラスの作成が完了した。だが、Stageには登ることはできない。

---
+++ core/src/com/mygdx/mrnom/models/SnakePart.java
@@ -0,0 +1,13 @@
+package com.mygdx.mrnom.models;
+
+/**
+ * Snake(Mr.Nom) head and body
+ */
+public class SnakePart {
+    public int x, y;
+
+    public SnakePart(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ core/src/com/mygdx/mrnom/models/World.java
@@ -0,0 +1,61 @@
+package com.mygdx.mrnom.models;
+
+import java.util.Random;
+
+/**
+ * World
+ */
+public class World {
+
+    /** Worldの幅 */
+    static final int WORLD_WIDTH = 10;
+    /** Worldの高さ */
+    static final int WORLD_HEIGHT = 13;
+    /** Stainを食べた時のスコアの加算値 */
+    static final int SCORE_INCREMENT = 10;
+    /** 時間間隔 初期値 */
+    static final float TICK_INITIAL = 0.5f;
+    /** 時間間隔 減算値 */
+    static final float TICK_DECREMENT = 0.05f;
+
+    public Snake snake;
+    public Stain stain;
+    public boolean isGameOver = false;
+    public int score = 0;
+
+    boolean fields[][] = new boolean[WORLD_WIDTH][WORLD_HEIGHT];
+    Random random = new Random(System.currentTimeMillis());
+    float tickTime = 0;
+    static float tick = TICK_INITIAL;
+
+    public World() {
+        snake = new Snake();
+        placeStain();
+    }
+
+    private void placeStain() {
+    }
+
+    public void update(float deltaTime) {
+        if (isGameOver) {
+            return;
+        }
+
+        tickTime += deltaTime;
+
+        while (tickTime > tick) {
+            tickTime -= tick;
+            snake.advance();;
+            if (snake.checkBitten()) {
+                isGameOver = true;
+                return;
+            }
+
+
+
+
+        }
+
+
+    }
+}
